a2dfd05942f3e1fb6e98f923ce5383c346d037f1
=DamjanRadev <radevdamjan@gmail.com>
2019-05-16 14:45:14 +0200

    Removed path helper

--- routes/persons.js
+++ routes/persons.js
@@ -1,9 +1,6 @@
-const path = require('path');
 
 const express = require('express');
 const router = express.Router();
-
-const rootDir = require('../helpers/path')
 
 
 router.get('/', (req, res, next) => {
