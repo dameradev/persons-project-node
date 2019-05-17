b0e90c699d29c8f94309f10d69232c29c27beea4
=DamjanRadev <radevdamjan@gmail.com>
2019-05-17 16:01:50 +0200

    Added routes for meeting

--- routes/meeting.js
+++ routes/meeting.js
@@ -4,3 +4,9 @@ const router = express.Router();
 const meetingsController = require('../controllers/meeting');
 
 router.get('/', meetingsController.getMeetings);
+
+router.get('/create-meeting', meetingsController.getCreateMeeting);
+
+router.post('/create-meeting', meetingsController.postCreateMeeting);
+
+module.exports = router;
