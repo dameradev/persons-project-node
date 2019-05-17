ecc102c2447806c958ef62d8623d77cf6ef15958
=DamjanRadev <radevdamjan@gmail.com>
2019-05-17 16:02:06 +0200

    Added controller for meeting

--- controllers/meeting.js
+++ controllers/meeting.js
@@ -1,7 +1,34 @@
-exports.getMeetings = (req, res, next) => {
+const Meeting = require('../models/meeting');
 
+exports.getMeetings = async (req, res, next) => {
+  const meetings = await Meeting.findAll({});
+  res.render('meetings/meeting-list', {
+    pageTitle: 'List of meetings',
+    path:'/meetings',
+    meetings
+  });
 }
 
 exports.getCreateMeeting = (req, res, next) => {
+  res.render('meetings/create-meeting', {
+    pageTitle:'Create a meeting',
+    path: '/create-meeting'
+  })
+}
+
+exports.postCreateMeeting = (req, res, next) => {
+  const start = req.body.start;
+  const duration = req.body.duration;
+  const description = req.body.description;
+  Meeting.create({
+    start,
+    duration,
+    description
+  })
+  .then(result => {
+    console.log('Created a meeting');
+    res.redirect('/meetings');
+  })
+  .catch(err=>console.log(err));
   
 }
